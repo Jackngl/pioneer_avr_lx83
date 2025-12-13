ba195c019627bf72ac75ff5810faad58560d4c87
Jack Ngl <jackn@hotmail.com>
2025-12-13 04:20:27 +0100

    errrr

--- custom_components/pioneer_avr_lx83/__init__.py
+++ custom_components/pioneer_avr_lx83/__init__.py
@@ -20,8 +20,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    # Utiliser async_create_task pour éviter les appels bloquants
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    )
 
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
 
@@ -37,4 +41,3 @@ async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Reload config entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
-
